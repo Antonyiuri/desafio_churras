302b57ba1d649a83030108a2dd6a18e0762e6d8d
Antony Iuri <143660042+Antonyiuri@users.noreply.github.com>
2024-03-13 11:41:58 -0300

    Desafio 1
    Desafio do churras

---
+++ desafio_01/churras.py
@@ -0,0 +1,17 @@
+#Desafio com If, Elif, Else 
+
+'''
+Crie um programa que dependendo da temperatura (em celcius) do stake 
+ele retorna o ponto do cozimento em portugues. O usuário deverá 
+fornocer a temperatura.
+
+Temperaturas - cozimento
+120°F ou 48°C - Rare (Selada)
+130°F ou 54°C - Medium rare (Selada)
+140°F ou 60°C - Medium (Ao ponto)
+150°F ou 65°C - Medium Well done (Ao ponto para o bem)
+160°F ou 71°C - Well done (Bem passada)
+'''
+
+temperatura = input
+print("Qual é a temperatura da carne?")
